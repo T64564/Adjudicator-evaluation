df96a1e454976aa30fd56bdd0af4699d9820f7e2
T65464 <a91381@gmail.com>
2016-10-23 19:17:40 +0900

    Remove unused included file

--- tests/AdjudicatorTest.php
+++ tests/AdjudicatorTest.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-use Illuminate\Support\Facades\Artisan;
 use App\Http\Models\Adjudicator;
 
 class AdjudicatorTest extends TestCase {
