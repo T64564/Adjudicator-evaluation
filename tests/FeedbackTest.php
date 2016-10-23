f0dbd01eec864c0e12a55667ecf0a39d714d9b11
T65464 <a91381@gmail.com>
2016-10-23 19:19:12 +0900

    Add FeedbackTest.php

---
+++ tests/FeedbackTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Http\Models\Feedback;
+
+class FeedbackTest extends TestCase {
+    public function setUp() {
+        parent::setUp();
+    }
+}
