d9eac9ed18676cb56da031c675963b601c2b9a60
T65464 <a91381@gmail.com>
2016-10-23 19:13:01 +0900

    Refactor validation test func

--- tests/AdjudicatorTest.php
+++ tests/AdjudicatorTest.php
@@ -32,57 +32,35 @@ class AdjudicatorTest extends TestCase {
     }
 
     public function testValidationCreate() {
-        $name  = '';
-        $score = 5;
-        $this->visit('/adjudicators/create')
-            ->type($name, 'name')
-            ->type($score, 'test_score')
-            ->check('active')
-            ->press('Create')
-            ->seePageIs('/adjudicators/create');
-        $this->dontSeeInDatabase('adjudicators', ['name' => $name, 'test_score' => $score]);
+        $names = ['', 'AAAA', 'AAAA'];
+        $scores = [1, -1, 'a'];
 
-        $name  = 'XXXX';
-        $score = -1;
-        $this->visit('/adjudicators/create')
-            ->type($name, 'name')
-            ->type($score, 'test_score')
-            ->check('active')
-            ->press('Create')
-            ->seePageIs('/adjudicators/create');
-        $this->dontSeeInDatabase('adjudicators', ['name' => $name, 'test_score' => $score]);
-
-        $name  = 'XXXX';
-        $score = 'a';
-        $this->visit('/adjudicators/create')
-            ->type($name, 'name')
-            ->type($score, 'test_score')
-            ->check('active')
-            ->press('Create')
-            ->seePageIs('/adjudicators/create');
-        $this->dontSeeInDatabase('adjudicators', ['name' => $name, 'test_score' => $score]);
+        for ($i = 0; $i < count($names); $i++) {
+            $this->visit('/adjudicators/create')
+                ->type($names[$i], 'name')
+                ->type($scores[$i], 'test_score')
+                ->check('active')
+                ->press('Create')
+                ->seePageIs('/adjudicators/create');
+            $this->dontSeeInDatabase('adjudicators',
+                ['name' => $names[$i], 'test_score' => $scores[$i]]);
+        }
     }
 
     public function testValidationEdit() {
         Adjudicator::create(['name' => 'AAAA', 'test_score' => 5, 'active' => true]);
-        $name  = '';
-        $score = 5;
-        $this->visit('/adjudicators/1/edit')
-            ->type($name, 'name')
-            ->type($score, 'test_score')
-            ->check('active')
-            ->press('Edit')
-            ->seePageIs('/adjudicators/1/edit');
-        $this->dontSeeInDatabase('adjudicators', ['name' => $name, 'test_score' => $score]);
+        $names = ['', 'AAAA', 'AAAA'];
+        $scores = [1, -1, 'a'];
 
-        $name  = 'XXXX';
-        $score = 'a';
-        $this->visit('/adjudicators/1/edit')
-            ->type($name, 'name')
-            ->type($score, 'test_score')
-            ->check('active')
-            ->press('Edit')
-            ->seePageIs('/adjudicators/1/edit');
-        $this->dontSeeInDatabase('adjudicators', ['name' => $name, 'test_score' => $score]);
+        for ($i = 0; $i < count($names); $i++) {
+            $this->visit('/adjudicators/1/edit')
+                ->type($names[$i], 'name')
+                ->type($scores[$i], 'test_score')
+                ->check('active')
+                ->press('Edit')
+                ->seePageIs('/adjudicators/1/edit');
+            $this->dontSeeInDatabase('adjudicators',
+                ['name' => $names[$i], 'test_score' => $scores[$i]]);
+        }
     }
 }
