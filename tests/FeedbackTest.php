b98246bacc7756b02fe6135c6ef47c30201a20a5
T65464 <a91381@gmail.com>
2016-10-25 00:24:30 +0900

    Add validation test

--- tests/FeedbackTest.php
+++ tests/FeedbackTest.php
@@ -13,47 +13,79 @@ class FeedbackTest extends TestCase {
     public function setUp() {
         parent::setUp();
         $this->round = Round::create(['name' => 'Round1', 'silent' => false]);
-        $this->team = Team::create(['name' => 'teamA', 'active' => true]);
-        $this->adjudicator = 
-            Adjudicator::create(['name' => 'AAAA', 'test_score' => 5, 'active' => true]);
+        $this->team1 = Team::create(['name' => 'team1', 'active' => true]);
+        $this->team2 = Team::create(['name' => 'team2', 'active' => true]);
+        $this->adjudicator1 = 
+            Adjudicator::create(['name' => 'A1', 'test_score' => 5, 'active' => true]);
 
         $this->adjudicator2 = 
-            Adjudicator::create(['name' => 'BBBB', 'test_score' => 5, 'active' => true]);
+            Adjudicator::create(['name' => 'A2', 'test_score' => 5, 'active' => true]);
     }
 
-    public function testPostFeedback() {
+    public function testCreate() {
         $this->visit('/feedbacks/1/create_team_to_adj')
-            ->select($this->team->id, 'evaluator_id')
-            ->select($this->adjudicator->id, 'evaluatee_id')
+            ->select($this->team1->id, 'evaluator_id')
+            ->select($this->adjudicator1->id, 'evaluatee_id')
             ->type(10, 'score')
             ->press('Create')
             ->seePageIs('feedbacks/1/enter_results');
         $this->seeInDatabase('feedbacks', 
             ['type' => 0, 'round_id' => $this->round->id,
-            'evaluator_id' => $this->team->id, 'evaluatee_id' => $this->adjudicator->id,
+            'evaluator_id' => $this->team1->id, 'evaluatee_id' => $this->adjudicator1->id,
             'score' => 10]);
         $this->visit('/feedbacks/1/create_team_to_adj')
-            ->select($this->team->id, 'evaluator_id')
-            ->select($this->adjudicator->id, 'evaluatee_id')
+            ->select($this->team1->id, 'evaluator_id')
+            ->select($this->adjudicator1->id, 'evaluatee_id')
             ->type(10, 'score')
             ->press('Create')
             ->seePageIs('feedbacks/1/create_team_to_adj');
 
         $this->visit('/feedbacks/1/create_adj_to_adj')
-            ->select($this->adjudicator->id, 'evaluator_id')
+            ->select($this->adjudicator1->id, 'evaluator_id')
             ->select($this->adjudicator2->id, 'evaluatee_id')
             ->type(10, 'score')
             ->press('Create')
             ->seePageIs('feedbacks/1/enter_results');
         $this->seeInDatabase('feedbacks', 
             ['type' => 1, 'round_id' => $this->round->id,
-            'evaluator_id' => $this->adjudicator->id, 'evaluatee_id' => $this->adjudicator2->id,
+            'evaluator_id' => $this->adjudicator1->id, 'evaluatee_id' => $this->adjudicator2->id,
             'score' => 10]);
         $this->visit('/feedbacks/1/create_adj_to_adj')
-            ->select($this->adjudicator->id, 'evaluator_id')
+            ->select($this->adjudicator1->id, 'evaluator_id')
             ->select($this->adjudicator2->id, 'evaluatee_id')
             ->type(10, 'score')
             ->press('Create')
             ->seePageIs('feedbacks/1/create_adj_to_adj');
+    }
+
+    public function testValidation() {
+        Feedback::create([
+            'type' => 0, 'round_id' => 1, 
+            'evaluator_id' => $this->team1->id, 
+            'evaluatee_id' => $this->adjudicator1->id, 'score' => 5]);
+        $this->assertEquals(1, Feedback::count());
+
+        /* a team submit twice */
+        $this->visit('/feedbacks/1/create_team_to_adj')
+            ->select($this->team1->id, 'evaluator_id')
+            ->select($this->adjudicator1->id, 'evaluatee_id')
+            ->type(10, 'score')
+            ->press('Create')
+            ->seePageIs('feedbacks/1/create_team_to_adj');
+        $this->assertEquals(1, Feedback::count());
+
+
+        Feedback::create([
+            'type' => 1, 'round_id' => 1,
+            'evaluator_id' => $this->adjudicator1->id,
+            'evaluatee_id' => $this->adjudicator2->id, 'score' => 5]);
+        /* combi (type, adj1 -> adj2) is unique*/
+        $this->visit('/feedbacks/1/create_adj_to_adj')
+            ->select($this->adjudicator1->id, 'evaluator_id')
+            ->select($this->adjudicator2->id, 'evaluatee_id')
+            ->type(10, 'score')
+            ->press('Create')
+            ->seePageIs('feedbacks/1/create_adj_to_adj');
+        $this->assertEquals(2, Feedback::count());
     }
 }
