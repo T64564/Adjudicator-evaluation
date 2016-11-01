0b419743fd84daf327475a8da008f95ffa51732e
T65464 <a91381@gmail.com>
2016-11-01 11:36:04 +0900

    Add icon on navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,51 +1,59 @@
 <nav class="navbar navbar-default">
-<div class="container">
-    <div class="navbar-header">
-        <!-- スマホやタブレットで表示した時のメニューボタン -->
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-    </div>
+    <div class="container">
+        <div class="navbar-header">
+            <!-- スマホやタブレットで表示した時のメニューボタン -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li>
-                {!! link_to_route('feedbacks.index', 'Feedback') !!}
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" 
-                            data-toggle="dropdown" role="button" aria-expanded="false">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href={{ route('feedbacks.index') }}>
+                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                        Feedback
+                    </a>
+                </li>
+                <li>
+
+                    <a href={{ route('results.ranking') }}>
+                    <i class="fa fa-trophy" aria-hidden="true"></i>
                     Results
-                    <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                    <li>
-                        {!! link_to_route('results.ranking', 'Ranking') !!}
-                    </li>
-                </ul>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" 
-                            data-toggle="dropdown" role="button" aria-expanded="false">
-                    Control Panel
-                    <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                    <li>
-                        {!! link_to_route('adjudicators.index', 'Adjudicators') !!}
-                    </li>
-                    <li>
-                        {!! link_to_route('teams.index', 'Teams') !!}
-                    </li>
-                    <li>
-                        {!! link_to_route('rounds.index', 'Rounds') !!}
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div><!-- /.navbar-collapse -->
-</div><!-- /.container-fluid -->
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" 
+                                data-toggle="dropdown" role="button" aria-expanded="false">
+                        <i class="fa fa-asterisk" aria-hidden="true"></i>
+                        Control Panel
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href={{ route('adjudicators.index') }}>
+                                <i class="fa fa-gavel" aria-hidden="true"></i>
+                                Adjudicators
+                            </a>
+                        </li>
+                        <li>
+                            <a href={{ route('teams.index') }}>
+                                <i class="fa fa-users" aria-hidden="true"></i>
+                                Teams
+                            </a>
+                        </li>
+                        <li>
+                            <a href={{ route('rounds.index') }}>
+                                <i class="fa fa-comments" aria-hidden="true"></i>
+                                Rounds
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
