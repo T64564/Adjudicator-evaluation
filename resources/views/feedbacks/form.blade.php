670ed8c3f2b85b65b7fb4c464504814822576acf
T65464 <a91381@gmail.com>
2016-11-27 13:28:43 +0900

    Modify tabindex

--- resources/views/feedbacks/form.blade.php
+++ resources/views/feedbacks/form.blade.php
@@ -7,36 +7,40 @@
 <div class="form-group">
     {{ Form::label('type', 'Type:') }}
     {{ Form::select('type', $types,
-    null,
-    ['id' => 'type', 'class' => 'form-control']) }}
-    <!-- // ['id' => 'type', 'class' => 'form&#45;control',  -->
-    <!-- // 'tabindex' => '1']) }} -->
+            null,
+            ['id' => 'type', 'class' => 'form-control', 
+        'tabindex' => '1']) }}
 </div>
 <div class="form-group">
     {{ Form::label('evaluator_id', 'Evaluator:') }}
     @if ($type == 'team_to_adj') 
         {{ Form::select('evaluator_id', $team_names, 
             null,
-        ['id' => 'evaluater_id', 'class' => 'form-control']) }}
+            ['id' => 'evaluater_id', 'class' => 'form-control',
+        'tabindex' => '2']) }}
     @elseif ($type == 'adj_to_adj') 
         {{ Form::select('evaluator_id', $adj_names, 
             null,
-        ['id' => 'evaluater_id', 'class' => 'form-control']) }}
+            ['id' => 'evaluater_id', 'class' => 'form-control',
+        'tabindex' => '2']) }}
     @endif
 </div>
 <div class="form-group">
     {{ Form::label('evaluatee_id', 'Evaluatee:') }}
     {{ Form::select('evaluatee_id', $adj_names, 
         null,
-    ['class' => 'form-control']) }}
+        ['class' => 'form-control',
+    'tabindex' => '3']) }}
 </div>
 <div class="form-group">
     {{ Form::label('score', 'Score:') }}
     {{ Form::text('score', 
         null,
-    ['class' => 'form-control']) }}
+        ['class' => 'form-control',
+    'tabindex' => '4']) }}
 </div>
 <div class="form-group">
     {{ Form::submit($submitButton, 
-    ['class' => 'btn btn-primary form-control']) }}
+        ['class' => 'btn btn-primary form-control',
+    'tabindex' => '5']) }}
 </div>
